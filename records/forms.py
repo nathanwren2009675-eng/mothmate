e915acd0cfe9555728ef0c34a71b9c66fd8ac5f9
Nathan Wren <nathanwren2009675@gmail.com>
2026-09-13 22:54:18 +0100

    Use auto-detected fields

--- records/forms.py
+++ records/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from .models import MothSighting
 
-
 class MothSightingForm(forms.ModelForm):
     class Meta:
         model = MothSighting
-        fields = '__all__'  # ✅ Auto-includes EVERY field from your model
-        exclude = ['user']   # ✅ Except the user (we set it automatically)
+        fields = '__all__'
+        exclude = ['user']
