e9647d64121aed79fa98562bdb75c2675c7e58d2
Nathan Wren <nathanwren2009675@gmail.com>
2026-09-13 20:56:25 +0100

    Add STATIC_ROOT

--- mothmate/settings.py
+++ mothmate/settings.py
@@ -108,12 +108,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login settings
