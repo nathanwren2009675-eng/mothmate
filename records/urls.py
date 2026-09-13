ffec6a7210d0bfa2a287b65959103da6b35d5c09
Nathan Wren <nathanwren2009675@gmail.com>
2026-09-13 22:35:20 +0100

    Fix all URLs and links

--- records/urls.py
+++ records/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-sighting/', views.add_sighting, name='add_sighting'),
+    path('my-records/', views.my_records, name='my_records'),
+    path('legacy-import/', views.legacy_import, name='legacy_import'),
 ]
